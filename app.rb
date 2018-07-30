5bf90bec3a8b67e1d5ddf567738f7a469ca40db8
Nicci Sheets <niccisheets@aol.com>
2018-07-30 16:33:30 -0400

    fixed app rb

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ get '/' do
 end
 
 post '/pizza_options' do
-	delivery_option = session[:delivery_option]
-	pizza_quantity = session[:pizza_quantity]
-	pizza_crust = session[:pizza_crust]
-	pizza_toppings = session[:pizza_toppings]
+	session[:delivery_option] = params[:delivery_option]
+	session[:pizza_quantity] = params[:pizza_quantity]
+	session[:pizza_crust] = params[:pizza_crust]
+	session[:pizza_toppings] = params[:pizza_toppings]
 	p "params are #{params}"
 	p "delivery session is #{session[:delivery_option]}"
 	redirect '/total'
@@ -20,13 +20,12 @@ post '/pizza_options' do
 end
 
 get '/total' do
-session[:delivery_option] = params[:delivery_option]
-	session[:pizza_quantity] = params[:pizza_quantity]
-	session[:pizza_crust] = params[:pizza_crust]
-	session[:pizza_toppings] = params[:pizza_toppings]
-	p "params are #{params}"
-session[:final_total] = final_total(params[:pizza_toppings], params[:pizza_crust], params[:pizza_quantity], params[:delivery_option])
-	erb :total, locals:{final_total: final_total, pizza_toppings: pizza_toppings, pizza_crust: pizza_crust, pizza_quantity: pizza_quantity, delivery_options: delivery_options}
+	delivery_option = session[:delivery_option]
+	pizza_quantity = session[:pizza_quantity]
+	pizza_crust = session[:pizza_crust]
+	pizza_toppings = session[:pizza_toppings]
+session[:final_total] = final_total(pizza_toppings, pizza_crust, pizza_quantity, delivery_option)
+	erb :total, locals:{final_total: session[:final_total], pizza_toppings: pizza_toppings, pizza_crust: pizza_crust, pizza_quantity: pizza_quantity, delivery_option: delivery_option}
 end
 
 
