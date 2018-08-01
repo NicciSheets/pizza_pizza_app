bad5f2eb676d0298c44d989c7f38c4b5e7fe38fe
Nicci Sheets <niccisheets@aol.com>
2018-08-01 15:26:23 -0400

    sizes

--- app.rb
+++ app.rb
@@ -10,16 +10,17 @@ end
 
 post '/pizza_options' do
 	delivery_option = params[:delivery_option]
+	pizza_size  = params[:pizza_size]
 	pizza_quantity = params[:how_many]
 	pizza_crust = params[:crust_type]
 	pizza_toppings = params[:toppings].to_s
 	p "params in pizza options are #{params}"
-	redirect '/total_page?delivery_option=' + delivery_option + '&pizza_quantity=' + pizza_quantity + '&pizza_crust=' + pizza_crust + '&pizza_toppings=' + pizza_toppings
+	redirect '/total_page?delivery_option=' + delivery_option + '&pizza_size=' + pizza_size + '&pizza_quantity=' + pizza_quantity + '&pizza_crust=' + pizza_crust + '&pizza_toppings=' + pizza_toppings
 end
 
 get '/total_page' do
 	p "params in total #{params}"
-	total = final_total(params[:pizza_toppings], params[:pizza_crust], params[:pizza_quantity], params[:delivery_option])
+	total = final_total(params[:pizza_toppings], params[:pizza_crust], params[:pizza_quantity], params[:delivery_option], params[:pizza_size])
 	p "total is #{total}"
 		erb :total, locals:{total: total} 
 end
--- pizza_app.rb
+++ pizza_app.rb
@@ -7,9 +7,9 @@ def crust_subtotal(pizza_crust)
 	crust_subtotal_arr = []
 	crust = "#{pizza_crust}"
 	if crust == "pan crust" 
-		crust_subtotal_arr << 8.00
+		crust_subtotal_arr << 0.50
 	else 
-		crust_subtotal_arr << 6.00
+		crust_subtotal_arr << 1.00
 	end
 	crust_subtotal_arr
 end
@@ -34,8 +34,23 @@ def toppings(pizza_toppings)
 	 # p "toppings are #{toppings(pizza_toppings)}"
 end
 
+def size(pizza_size)
+	size_array = []
+	if pizza_size == "small"
+		size_array << 5.00
+	elsif pizza_size == "medium"
+		size_array << 7.00
+	else 
+		size_array << 9.00
+	end
+	puts size_array
+	size_array
+end
+
+
 
 def subtotal_array(pizza_toppings, pizza_crust)
+	puts "pizza_toppings is #{pizza_toppings} and pizza_crust is #{pizza_crust}"
 	new_array = []
 	new_array = toppings(pizza_toppings) << crust_subtotal(pizza_crust)
 	puts "new_array is #{new_array} in subtotal_array"
@@ -57,20 +72,28 @@ end
 
 #p "delivery charge is #{delivery(delivery_option)}"
 
-def final_total(pizza_toppings, pizza_crust, pizza_quantity, delivery_option)
+def final_total(pizza_toppings, pizza_crust, pizza_quantity, delivery_option, pizza_size)
 	final_hash = {}
 	puts "in final total pizza_toppings is #{pizza_toppings}"
 	total_arr = []
 	taxes = 0.06
-	total_arr = subtotal_array(pizza_toppings, pizza_crust) * pizza_quantity.to_f
+	puts "pizza_quantity is #{pizza_quantity}"
+	subtotal_arr = subtotal_array(pizza_toppings, pizza_crust)
+	puts "subtotal_arr #{subtotal_arr}"
+	total_arr = subtotal_arr * pizza_quantity.to_f
 	p "total_arr is #{total_arr}"
 	tax_and_delivery = (total_arr * taxes) + delivery(delivery_option)
 	# p "#{total_arr * taxes}"
 	# p "tax worksheet is #{tax_and_delivery}"
-	total_arr << tax_and_delivery
+	# puts "pizza size is #{size} and its class is #{size.class}"	
+	total_arr << tax_and_delivery 
+	my_size = size(pizza_size)
+	total_arr << my_size
+
 	puts "in final_total total_arr is #{total_arr} class is #{total_arr.class}"
 	total = total_arr.flatten.sum
 	final_hash["pizza_quantity"] = pizza_quantity
+	final_hash["pizza_size"] = pizza_size
 	final_hash["pizza_crust"] = pizza_crust
 	final_hash["pizza_toppings"] = pizza_toppings
 	final_hash["delivery_option"] = delivery_option
