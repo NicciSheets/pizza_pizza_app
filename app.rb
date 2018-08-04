4095b62fb3b8c8059b5acaa526f3b125ebb3d028
Nicci Sheets <niccisheets@aol.com>
2018-08-03 20:46:44 -0400

    got confirmation page set up and trying to pass total through sessions to total page

--- app.rb
+++ app.rb
@@ -16,11 +16,11 @@ post '/pizza_options' do
 	pizza_crust = params[:crust_type]
 	pizza_toppings = params[:toppings].to_s
 	p "params in pizza options are #{params}"
-	redirect '/total_page?delivery_option=' + delivery_option + '&pizza_size=' + pizza_size + '&pizza_quantity=' + pizza_quantity + '&pizza_crust=' + pizza_crust + '&pizza_toppings=' + pizza_toppings
+	redirect '/confirm_page?delivery_option=' + delivery_option + '&pizza_size=' + pizza_size + '&pizza_quantity=' + pizza_quantity + '&pizza_crust=' + pizza_crust + '&pizza_toppings=' + pizza_toppings
 end
 
-get '/total_page' do
-	p "params in total #{params}"
+get '/confirm_page' do
+	p "params in cofirm page get #{params}"
 	total = final_total(params[:pizza_toppings], params[:pizza_crust], params[:pizza_quantity], params[:delivery_option], params[:pizza_size])
 	p_quantity = total.values[0]
 	p_size = total.values[1]
@@ -30,7 +30,36 @@ get '/total_page' do
 	p "p_toppings is #{p_toppings} and its class is #{p_toppings.class}"
 	p_delivery = total.values[4]
 	p_total = total.values[5]
-	erb :total, locals:{total: total, p_quantity: p_quantity, p_size: p_size, p_crust: p_crust, toppings: toppings, p_delivery: p_delivery, p_total: p_total} 
+	p_session = session[:p_total]
+	p "p_total session is #{p_total}"
+	erb :confirm, locals:{total: total, p_quantity: p_quantity, p_size: p_size, p_crust: p_crust, toppings: toppings, p_delivery: p_delivery, p_session: p_session} 
 end
 
+post '/confirm_page' do
+	confirm_quantity = params[:confirm_quantity]
+	confirm_size = params[:confirm_size]
+	confirm_crust = params[:confirm_crust]
+	confirm_topping = params[:confirm_topping]
+	confirm_delivery = params[:confirm_delivery]
+	p "confirm_quantity is #{confirm_quantity} and its class is #{confirm_quantity.class}"
+	confirm_arr = []
+	confirm_arr << confirm_quantity << confirm_size << confirm_crust << confirm_topping << confirm_delivery
+	p "confirm arr is #{confirm_arr} and its class is #{confirm_arr.class}"
+	confirm_arr.each do |confirm|
+		if confirm == "yes"
+			redirect '/total_page'
+		else
+			redirect '/pizza_options'
+		end
+	end
+end	
+
+get '/pizza_options' do
+	erb :options
+end
+
+get '/total_page' do
+	p_session = params[:p_session]
+	erb :total, locals:{p_session: p_session}
+end
 
